63f79c0c0cdbd8cefbcbb8f06af11589a4143fb3
megumikitty <mary.moody010@gmail.com>
2017-02-15 16:53:03 -0600

    set gmail app password

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,7 +4,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 	=>  '587',
   :authentication       	=>  :plain,
   :user_name            	=>  'mary@weddingmemories.us',
-  :password             	=>  'Alexzander12',
+  :password             	=>  'mgbcjbwtppaoapwj',
   :domain               	=>  'weddingmemories.us',
   :enable_starttls_auto  	=>  true
 }
